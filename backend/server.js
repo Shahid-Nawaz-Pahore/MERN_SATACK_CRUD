2e34c3984fc59e8d8d508b18c6254a701b677471
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2023-11-08 00:08:17 -0800

    connect mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 //
 const express=require("express");
+const mongoose = require('mongoose');
+
 const app=express();
 const PORT=5000;
+mongoose.connect('mongodb://127.0.0.1:27017/mern_stack_crud').then(()=>{
+    console.log("DB connection created successfully");
+}).catch((error)=>{
+    console.log(error)
+});
 
 app.get("/",(req,res)=>{
   res.send("this is basic route");
